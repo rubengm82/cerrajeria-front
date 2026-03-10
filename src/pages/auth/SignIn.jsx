01c8859a3f68fb30b3555d158cd963e0c4e7f365
ruben <ruben.gallardo@inslapineda.cat>
2026-03-10 13:17:57 +0100

    Primera implementacion de SCSS

--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { useAuth } from '../../context/AuthContext'
+import './SignIn.scss'
 
 function SignIn() {
   const [email, setEmail] = useState('')

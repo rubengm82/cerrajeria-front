c62b5485bb0a0cfd840cc092747f9d761bb680b5
Ale2484 <alejandrotp86@gmail.com>
2026-03-23 21:29:42 +0100

    Cambio de posicion de filtros a modal

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { getProducts } from "../../api/products_api"
 import LoadingAnimation from "../../components/LoadingAnimation"
 import ProductCard from "../../components/ProductCard"
-import { HiOutlineAdjustmentsHorizontal, HiMagnifyingGlass } from "react-icons/hi2";
+import { HiOutlineAdjustmentsHorizontal, HiMagnifyingGlass, HiXMark, HiOutlineFunnel } from "react-icons/hi2";
 import { getCategories } from "../../api/categories_api";
 import '../../../scss/main_shop.scss'
 
@@ -36,6 +36,9 @@ function Products() {
     )
   }
 
+  const filteredProducts = products.filter((product) => (
+    selectedCategories.length === 0 || selectedCategories.includes(product.category?.name)
+  ))
 
   return loading ? <LoadingAnimation /> : (
     <div className='products-page'>
@@ -47,55 +50,80 @@ function Products() {
               <h2 className="products-top__title">Productes</h2>
             </div>
 
-            <p className="products-top__count text-base-400">
-              Mostrant {products.filter((product) => (selectedCategories.length === 0 || selectedCategories.includes(product.category?.name))).length} productes
-            </p>
+            <div className="products-top__actions">
+              <p className="products-top__count text-base-400">
+                Mostrant {filteredProducts.length} productes
+              </p>
+
+              <button type="button" className="btn products-top__filters-button" onClick={() => document.getElementById("products-filters-modal").showModal()}>
+                <HiOutlineAdjustmentsHorizontal className="filters-box__icon" />
+                Filtres
+              </button>
+            </div>
           </div>
 
           <div className="products-layout">
-            <aside className="filters-box simple-container">
-              <div className="filters-box__head">
-                <HiOutlineAdjustmentsHorizontal className="filters-box__icon" />
-                <h3 className="filters-box__title">Filtres</h3>
-              </div>
-
-              <div className="filters-box__content">
-                <div className="collapse collapse-arrow filters-box__section border-base-300">
-                  <input type="checkbox" defaultChecked />
-                  <div className="collapse-title filters-box__section-title">
-                    <h4 className="filters-box__label">Categories</h4>
-                  </div>
-
-                  <div className="collapse-content filters-box__section-body">
-                    <label className="input filters-box__search">
-                      <HiMagnifyingGlass className="filters-box__search-icon text-base-300" />
-                      <input type="search" placeholder="Buscar categoria.." />
-                    </label>
-
-                    <div className="filters-box__list">
-                      {categories.map((category) => (
-                        <label key={category.name} className="filters-box__item">
-                          <input type="checkbox" className="checkbox checkbox-md border-base-300" checked={selectedCategories.includes(category.name)} onChange={() => toggleCategory(category.name)}/>
-                          <span className="filters-box__item-name">{category.name}</span>
-                          <span className="filters-box__item-count text-base-400">({category.products?.length})</span>
-                        </label>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </aside>
-
             <div>
               <div className="products-list">
-                { products.filter((product) => (selectedCategories.length === 0 || selectedCategories.includes(product.category?.name))).length > 0 ?
-                  products.filter((product) => (selectedCategories.length === 0 || selectedCategories.includes(product.category?.name))).map((product) => (
+                { filteredProducts.length > 0 ?
+                  filteredProducts.map((product) => (
                   <ProductCard key={product.id} product={product} />
                   )) :
                 <p className='products-empty'>Actualment no hi ha productes</p>}
               </div>
             </div>
           </div>
+
+          <dialog id="products-filters-modal" className="modal modal-bottom sm:modal-middle">
+            <div className="modal-box filters-modal">
+              <div className="filters-box">
+                <div className="filters-box__head">
+                  <div className="filters-box__head-content">
+                    <HiOutlineFunnel className="filters-box__icon" />
+                    <h3 className="filters-box__title">Filtres</h3>
+                  </div>
+                  <div className="modal-action filters-box__modal-close">
+                    <form method="dialog">
+                      <button>
+                        <HiXMark className="filters-box__icon filters-box__close-icon" />
+                      </button>
+                    </form>
+                  </div>
+                </div>
+
+                <div className="filters-box__content">
+                  <div className="collapse collapse-arrow filters-box__section border-base-300">
+                    <input type="checkbox" defaultChecked />
+                    <div className="collapse-title filters-box__section-title">
+                      <h4 className="filters-box__label">Categories</h4>
+                    </div>
+
+                    <div className="collapse-content filters-box__section-body">
+                      <label className="input filters-box__search">
+                        <HiMagnifyingGlass className="filters-box__icon text-base-300" />
+                        <input type="search" placeholder="Buscar categoria.." />
+                      </label>
+
+                      <div className="filters-box__list">
+                        {categories.map((category) => (
+                          <label key={category.name} className="filters-box__item">
+                            <input
+                              type="checkbox"
+                              className="checkbox checkbox-md border-base-300"
+                              checked={selectedCategories.includes(category.name)}
+                              onChange={() => toggleCategory(category.name)}
+                            />
+                            <span className="filters-box__item-name">{category.name}</span>
+                            <span className="filters-box__item-count text-base-400">({category.products?.length})</span>
+                          </label>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </dialog>
         </div>
       </div>
     </div>

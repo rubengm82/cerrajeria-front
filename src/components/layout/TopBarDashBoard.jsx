1a60ff2c4c21e5068a7119acf0a48b790d2ca39f
ruben <ruben.gallardo@inslapineda.cat>
2026-04-12 12:12:08 +0200

    Quitado informacion del boton USER del topbar, solo botones para cerrar sesion o inciar.

--- src/components/layout/TopBarDashBoard.jsx
+++ src/components/layout/TopBarDashBoard.jsx
@@ -1,9 +1,10 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { useAuth } from '../../context/AuthContext'
-import { HiBars3, HiOutlineUserCircle } from 'react-icons/hi2'
+import { HiBars3, HiOutlineUserCircle, HiOutlineHome } from 'react-icons/hi2'
 
 export default function TopBarDashBoard() {
   const { user, logout } = useAuth()
+  const location = useLocation()
   
   return (
     <div className="navbar bg-transparent">
@@ -23,20 +24,43 @@ export default function TopBarDashBoard() {
             <HiOutlineUserCircle className="shop-tobar-end__icon" aria-hidden="true" />
           </button>
           <ul tabIndex={-1} className="mt-3 z-50 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-            <li className="menu-title">
-              <span>{user?.name || 'Usuari'}</span>
-            </li>
-            <li><a>{user?.email || 'email@exemple.com'}</a></li>
-            <li>
-              <a>
-                {user?.role === 'admin' || user?.role === 1
-                  ? 'Administrador'
-                  : 'Usuari'}
-              </a>
-            </li>
-            <li className="border-t border-base-300 mt-2 pt-2">
-              <button className='btn btn-error w-full' onClick={logout} aria-label="Tancar la sessió actual">Tancar sessió</button>
-            </li>
+            {location.pathname !== '/admin/dashboard' && (
+              <>
+                <li className="menu-title">
+                  <span>{user?.name || 'Usuari'}</span>
+                </li>
+                <li><a>{user?.email || 'email@exemple.com'}</a></li>
+                <li>
+                  <a>
+                    {user?.role === 'admin' || user?.role === 1
+                      ? 'Administrador'
+                      : 'Usuari'}
+                  </a>
+                </li>
+                <li className="border-t border-base-300 mt-2 pt-2 mb-2">
+                  <Link to="/" className="btn btn-primary btn-outline w-full">
+                    <HiOutlineHome className="size-6" aria-hidden="true" />
+                    Tornar a la botiga
+                  </Link>
+                </li>
+                <li>
+                  <button className='btn btn-error w-full' onClick={logout} aria-label="Tancar la sessió actual">Tancar sessió</button>
+                </li>
+              </>
+            )}
+            {location.pathname === '/admin/dashboard' && (
+              <>
+                <li className="mb-2">
+                  <Link to="/" className="btn btn-primary btn-outline w-full">
+                    <HiOutlineHome className="size-6" aria-hidden="true" />
+                    Tornar a la botiga
+                  </Link>
+                </li>
+                <li>
+                  <button className='btn btn-error w-full' onClick={logout} aria-label="Tancar la sessió actual">Tancar sessió</button>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
